0a372eb8d90ddedf17f4915d55228fa8b47ca9be
mengpeiyao <345004536@qq.com>
2017-07-31 21:28:00 +0800

    页面分为三块

---
+++ src/components/Achingsoul.vue
@@ -0,0 +1,16 @@
+<template>
+  <dl id="achingsoul">
+      <dt>姓名</dt>
+      <dd>佩佩</dd>
+      <dt>年龄</dt>
+      <dd>23</dd>
+      <dt>性别</dt>
+      <dd>女</dd>
+  </dl>
+</template>
+
+<style>
+  #achingsoul{
+      color: red;
+  }
+</style>
